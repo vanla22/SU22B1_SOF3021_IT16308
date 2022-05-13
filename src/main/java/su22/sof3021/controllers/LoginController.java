93b30dbc1e4a74229eafe5e8efa5447a2086888d
HoangTien339 <tiennh21@fpt.edu.vn>
2022-05-13 12:09:56 +0700

    Lesson 2: Controller

---
+++ src/main/java/su22/sof3021/controllers/LoginController.java
@@ -0,0 +1,41 @@
+package su22.sof3021.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import su22.sof3021.beans.Login;
+
+@Controller
+public class LoginController {
+	@Autowired
+	HttpServletRequest request;
+
+	@GetMapping("login")
+	public void getLoginForm()
+	{
+		return ;
+	}
+	
+//	@PostMapping("login")
+//	public String login(
+//		@RequestParam("email") String email,
+//		@RequestParam("password") String password
+//	) {
+//		System.out.println(email);
+//		System.out.println(password);
+//		
+//		return "login";
+//	}
+	
+	@PostMapping("login")
+	public String login(Login login) {
+		System.out.println(login.getEmail());
+		System.out.println(login.getPassword());
+		return "redirect:login";
+	}
+}
---
+++ src/main/webapp/views/login.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>IT16308 - Login</title>
+</head>
+<body>
+	<form method="POST" action="/su22b1_it16308_sof3021/login">
+		<div>
+			<label>Email</label>
+			<input type="email" name="email" />
+		</div>
+		<div>
+			<label>Password</label>
+			<input type="password" name="password" />
+		</div>
+		<button>Login</button>
+	</form>
+</body>
+</html>
