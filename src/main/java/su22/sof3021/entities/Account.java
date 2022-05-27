739c8a064eb8d45063d94945aac8ea37b222f555
HoangTien339 <tiennh21@fpt.edu.vn>
2022-05-27 10:09:59 +0700

    Form Create Account

--- src/main/java/su22/sof3021/entities/Account.java
+++ src/main/java/su22/sof3021/entities/Account.java
@@ -17,25 +17,25 @@ public class Account {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	private int id;
-	
+
 	@Column(name="username")
 	private String username;
 
 	@Column(name="password")
 	private String password;
-	
+
 	@Column(name="fullname")
 	private String fullname;
-	
+
 	@Column(name="email")
 	private String email;
-	
+
 	@Column(name="photo")
 	private String photo;
-	
+
 	@Column(name="activated")
 	private int activated;
-	
+
 	@Column(name="admin")
 	private int admin;
 }
--- src/main/java/su22/sof3021/entities/Product.java
+++ src/main/java/su22/sof3021/entities/Product.java
@@ -1,6 +1,6 @@
 package su22.sof3021.entities;
 
-import java.sql.Date;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
